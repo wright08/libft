471c08b2d6d7402676b5bce4e07a4f32aa40c4bb
wright08 <wright08@vt.edu>
2019-03-07 16:33:54 -0800

    Make use of ft_strnew

--- src/ft_strsub.c
+++ src/ft_strsub.c
@@ -6,21 +6,18 @@
 /*   By: rwright <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 20:47:24 by rwright           #+#    #+#             */
-/*   Updated: 2019/01/30 19:43:36 by rwright          ###   ########.fr       */
+/*   Updated: 2019/03/07 16:33:15 by rwright          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	*ft_strsub(char *s, unsigned int start, size_t len)
+char	*ft_strsub(char *str, unsigned int start, size_t len)
 {
-	char	*ptr;
+	char	*ret;
 
-	if ((ptr = malloc(len + 1)))
-	{
-		ft_memcpy(ptr, s + start, len);
-		ptr[len] = '\0';
-	}
-	return (ptr);
+	if ((ret = ft_strnew(len)))
+		ft_memcpy(ret, str + start, len);
+	return (ret);
 }
