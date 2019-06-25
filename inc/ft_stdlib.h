6176cb13d05776151786cddde3c7c958f6d1f3fa
wright08 <wright08@vt.edu>
2019-06-25 13:52:33 -0700

    Include

--- inc/ft_stdlib.h
+++ inc/ft_stdlib.h
@@ -6,7 +6,7 @@
 /*   By: rwright <rwright@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 15:42:04 by rwright           #+#    #+#             */
-/*   Updated: 2019/06/07 16:44:19 by rwright          ###   ########.fr       */
+/*   Updated: 2019/06/25 13:52:15 by rwright          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ char*	ft_itoa(int n);
 char*	ft_lltoa(long long n);
 char*	ft_ulltoa_base(unsigned long long n, int base);
 void*	ft_memalloc(size_t size);
+void	qsort(void *base, size_t nmemb, size_t size,
+			int (*compar)(const void *, const void *));
 
 #endif
